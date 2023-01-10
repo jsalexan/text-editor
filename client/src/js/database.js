0566396b0ca8b1d89a60b45de58a4351aa1f01d3
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2023-01-09 19:59:41 -0800

    made a few updates to the package json to attempt to fix the storage bug.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,6 +1,7 @@
 import { openDB } from 'idb';
 
-const initdb = async () =>
+const initdb = async () => {
+  console.log('initdb initialized')
   openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
@@ -11,28 +12,27 @@ const initdb = async () =>
       console.log('jate database created');
     },
   });
+};
 
 export const putDb = async (content) => {
   console.log('PUT to the JATE database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: 1, value: content });
+  const request = store.put({ id: 1, jate: content });
   const result = await request;
-  console.log('🚀 - Data successfully saved to the database', result);
-  // console.error('putDb not implemented');
+  console.log('🚀 - Data successfully saved to the database', result.value);
 };
 
 export const getDb = async () => {
   console.log('GET all from the JATE database');
   const jateDb = await openDB('jate', 1);
+  console.log('Opened database successfully!');
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
-  console.log('result.value', result);
-  return result; 
-  // console.error('getDb not implemented');
+  console.log('Return result.value', result.value);
 };
 
 initdb();
